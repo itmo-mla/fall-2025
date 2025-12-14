2128cf44ea7b90885029b189d5a0b74956df5dcd
etlumac <105116609+etlumac@users.noreply.github.com>
2025-12-14 17:33:28 +0300

    fix

--- students/tlumach-ed/lab3/source/data_loader.py
+++ students/tlumach-ed/lab3/source/data_loader.py
@@ -1,4 +1,3 @@
-# загрузка и подготовка датасета
 import numpy as np
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
--- students/tlumach-ed/lab3/source/kernels.py
+++ students/tlumach-ed/lab3/source/kernels.py
@@ -4,3 +4,8 @@ import numpy as np
 def linear_kernel(x, y):
     return np.dot(x, y)
 
+def polynomial_kernel(x, y, degree=3, coef0=1):
+    return (np.dot(x, y) + coef0)**degree
+
+def rbf_kernel(x, y, gamma=0.5):
+    return np.exp(-gamma * np.linalg.norm(x - y)**2)
