1d81fb527b36fb0a7f02529b0f77206d9c58610a
etlumac <105116609+etlumac@users.noreply.github.com>
2026-01-16 12:23:57 +0300

    fix main and data_loader

--- students/tlumach-ed/lab3/source/data_loader.py
+++ students/tlumach-ed/lab3/source/data_loader.py
@@ -2,30 +2,33 @@ import numpy as np
 from sklearn.datasets import make_classification, make_moons
 from sklearn.model_selection import train_test_split
 
-# def load_dataset(n_samples=500, n_features=2, random_state=42):
-#     # Создаём бинарный датасет
-#     X, y = make_classification(
-#         n_samples=n_samples,
-#         n_features=n_features,
-#         n_informative=2,
-#         n_redundant=0,
-#         n_classes=2,
-#         n_clusters_per_class=1,
-#         flip_y=0.1,
-#         random_state=random_state
-#     )
-#     y = 2*y - 1  # преобразуем метки в {-1, +1}
-#     return train_test_split(X, y, test_size=0.3, random_state=random_state)
+def load_dataset(
+    dataset_type="classification",
+    n_samples=500,
+    n_features=2,
+    random_state=42
+):
+    if dataset_type == "classification":
+        X, y = make_classification(
+            n_samples=n_samples,
+            n_features=n_features,
+            n_informative=2,
+            n_redundant=0,
+            n_classes=2,
+            n_clusters_per_class=1,
+            flip_y=0.1,
+            random_state=random_state
+        )
 
+    elif dataset_type == "moons":
+        X, y = make_moons(
+            n_samples=n_samples,
+            noise=0.2,
+            random_state=random_state
+        )
 
-def load_dataset(n_samples=500, random_state=42):
-    X, y = make_moons(
-        n_samples=n_samples,
-        noise=0.2,
-        random_state=random_state
-    )
+    else:
+        raise ValueError("Unknown dataset_type")
 
     y = 2 * y - 1  # {0,1} → {-1,+1}
     return train_test_split(X, y, test_size=0.3, random_state=random_state)
-
-
--- students/tlumach-ed/lab3/source/main.py
+++ students/tlumach-ed/lab3/source/main.py
@@ -6,45 +6,51 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
-# Загружаем данные
-X_train, X_test, y_train, y_test = load_dataset()
 
-# Масштабирование (стандартное)
+DATASET_TYPE = "moons"           # "classification" | "moons"
+KERNEL_TYPE = "rbf"              # "linear" | "poly" | "rbf"
+C_VALUE = 1.0
+
+X_train, X_test, y_train, y_test = load_dataset(dataset_type=DATASET_TYPE)
+
+# Масштабирование
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Кастомная реализация
-#svm_custom = DualSVM(C=1.0, kernel=linear_kernel)
-#svm_custom = DualSVM(
-#    C=1.0,
-#    kernel=lambda x, y: polynomial_kernel(x, y, degree=3)
-#)
-svm_custom = DualSVM(
-    C=1.0,
-    kernel=lambda x, y: rbf_kernel(x, y, gamma=0.5)
-)
+if KERNEL_TYPE == "linear":
+    custom_kernel = linear_kernel
+    sklearn_kernel = "linear"
 
+elif KERNEL_TYPE == "poly":
+    custom_kernel = lambda x, y: polynomial_kernel(x, y, degree=3)
+    sklearn_kernel = "poly"
+
+elif KERNEL_TYPE == "rbf":
+    custom_kernel = lambda x, y: rbf_kernel(x, y, gamma=0.5)
+    sklearn_kernel = "rbf"
+
+else:
+    raise ValueError("Unknown kernel type")
+
+
+svm_custom = DualSVM(C=C_VALUE, kernel=custom_kernel)
 svm_custom.fit(X_train, y_train)
+
 y_pred_custom = svm_custom.predict(X_test)
 print("Custom SVM Accuracy:", accuracy_score(y_test, y_pred_custom))
 
-# sklearn SVM
-# svm_sklearn = SVC(C=1.0, kernel='linear')
-#svm_sklearn = SVC(C=1.0, kernel='poly', degree=3)
-svm_sklearn = SVC(C=1.0, kernel='rbf', gamma=0.5)
+
+svm_sklearn = SVC(
+    C=C_VALUE,
+    kernel=sklearn_kernel,
+    degree=3,
+    gamma=0.5
+)
 
 svm_sklearn.fit(X_train, y_train)
 y_pred_sklearn = svm_sklearn.predict(X_test)
 print("Sklearn SVM Accuracy:", accuracy_score(y_test, y_pred_sklearn))
 
-# Визуализация
-#plot_decision_boundary(svm_custom, X_train, y_train, kernel_type="Linear (Custom)")
-#plot_decision_boundary(svm_sklearn, X_train, y_train, kernel_type="Linear (Sklearn)")
-#plot_decision_boundary(svm_custom, X_train, y_train,kernel_type="Polynomial (Custom)")
-#plot_decision_boundary(svm_sklearn, X_train, y_train, kernel_type="Polynomial")
-plot_decision_boundary(svm_custom, X_train, y_train, kernel_type="RBF (Custom)")
-plot_decision_boundary(svm_sklearn, X_train, y_train, kernel_type="RBF")
-
-
-
+plot_decision_boundary(svm_custom, X_train, y_train, kernel_type=f"{KERNEL_TYPE} (Custom)")
+plot_decision_boundary(svm_sklearn, X_train, y_train, kernel_type=f"{KERNEL_TYPE} (Sklearn)")
