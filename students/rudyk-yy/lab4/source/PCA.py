4fb23fe48e539c0ee73e8f3793b6057fff4e95ea
yarikonen <hell03@List.ru>
2026-01-18 21:29:37 +0300

    lab5 fixes

--- students/rudyk-yy/lab4/source/PCA.py
+++ students/rudyk-yy/lab4/source/PCA.py
@@ -3,14 +3,20 @@ import numpy as np
 class PCA:
 
 
-    def __init__(self, n_components=None, eps=None):
+    def __init__(self, n_components=None, eps=None, use_zscore=False):
         self.n_components = n_components
         self.eps = eps
+        self.use_zscore = use_zscore
 
     def fit(self, X):
-        # центрирование
+        # z-score стандартизация
         self.mean_ = X.mean(axis=0)
-        Xc = X - self.mean_
+        self.std_ = X.std(axis=0)
+        self.std_[self.std_ == 0] = 1  
+        if self.use_zscore:
+            Xc = (X - self.mean_) / self.std_
+        else:
+            Xc = X - self.mean_
 
         # SVD
         V, S, U_T = np.linalg.svd(Xc, full_matrices=False)
@@ -28,12 +34,12 @@ class PCA:
                 self.n_components_ = X.shape[1]
             else:
                 total = self.explained_variance_.sum()
-                residual = np.array([
+                self.residual = np.array([
                     self.explained_variance_[m+1:].sum() / total
                     for m in range(len(self.explained_variance_))
                 ])
 
-                self.n_components_ = np.argmax(residual <= self.eps)
+                self.n_components_ = np.argmax(self.residual <= self.eps)
                 if self.n_components_ == 0:
                     self.n_components_ = 1
         else:
@@ -45,11 +51,17 @@ class PCA:
         return self
 
     def transform(self, X):
-        Xc = X - self.mean_
+        if self.use_zscore:
+            Xc = (X - self.mean_) / self.std_
+        else:
+            Xc = X - self.mean_
         return Xc @ self.components_.T
 
     def inverse_transform(self, Z):
-        return Z @ self.components_ + self.mean_
+        if self.use_zscore:
+            return Z @ self.components_ * self.std_ + self.mean_
+        else:
+            return Z @ self.components_ + self.mean_
 
     def fit_transform(self, X):
         self.fit(X)
