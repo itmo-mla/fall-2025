110e6bb5a99bc6786fd3c5d9cd12c5a5e5db91ad
ikasyutin <boxfighterilya@gmail.com>
2026-01-18 23:29:10 +0300

    add NR method

---
+++ students/kasiutin-ia/lab5/source/main.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+
+class MetricsEstimator:
+    def __init__(self):
+        self.accuracy = None
+        self.precision = None
+        self.recall = None
+        self.f1_score = None
+
+    def get_metrics(self, y_true: np.ndarray, y_pred: np.ndarray) -> None:
+        self.accuracy = self.get_accuracy(y_true, y_pred)
+        self.precision = self.get_precision(y_true, y_pred)
+        self.recall = self.get_recall(y_true, y_pred)
+        self.f1_score = self.get_f1_score(y_true, y_pred)
+
+    def get_accuracy(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return np.sum(y_true == y_pred) / len(y_true)
+
+    def get_precision(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        tp = np.sum((y_true == 1) * (y_pred == 1))
+        fp = np.sum((y_true == -1) * (y_pred == 1))
+        return tp / (tp + fp)
+
+    def get_recall(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        tp = np.sum((y_true == 1) * (y_pred == 1))
+        fn = np.sum((y_true == 1) * (y_pred == -1))
+        return tp / (tp + fn)
+
+    def get_f1_score(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        precision = self.get_precision(y_true, y_pred)
+        recall = self.get_recall(y_true, y_pred)
+        return 2 * precision * recall / (precision + recall)
+
+    def __str__(self):
+        return f"accuracy = {self.accuracy}\nprecision = {self.precision}\nrecall = {self.recall}\nf1_score = {self.f1_score}"
+
+
+class LogReg:
+    def __init__(self):
+        self.learning_rate = None
+        self.weights = None
+        self.Q = None
+
+
+    def fit(self, X: np.ndarray, y: np.ndarray, learning_rate: float = 0.2, n_iter: int = 10) -> None:
+        self.weights = self.weights = np.random.randn(X.shape[-1])
+
+        Q = []
+        prev_sigm = np.zeros_like(y)
+        for i in range(n_iter):
+            # sigma = self._sigmoid(self.weights @ X.T * y)
+
+            # grad = - np.linalg.inv(X.T @ np.diag((1 - sigma) * sigma) @ X + np.eye(X.shape[1]) * 1e-4) @ X.T @ (y / sigma)
+
+
+            # self.weights -= learning_rate * grad
+
+            # new_Q = self._get_Q(X, y)
+            # print(f"Iteration {i + 1}: Q = {new_Q}")
+            # Q.append(new_Q)
+
+            sigm = self._sigmoid(self.weights @ X.T * y)
+
+            gradient = -(X.T @ (y / sigm))
+            D = np.diag(sigm * (1 - sigm))
+            hessian = X.T @ D @ X
+
+            # Add regularization to prevent singular matrix
+            hessian += 1e-6 * np.eye(hessian.shape[0])
+
+            self.weights -= learning_rate * (np.linalg.inv(hessian) @ gradient)
+
+            upd_norm = np.linalg.norm(sigm - prev_sigm)
+            if upd_norm < 1e-3:
+                print(f"Converged at iteration {i + 1}")
+                break
+            else:
+                prev_sigm = sigm
+                print(f"-- NR iter {i + 1}: {upd_norm}")
+        return
+        return Q
+
+
+    def _get_Q(self, X: np.ndarray, y_true: np.ndarray) -> float:
+        margins = self.weights @ X.T * y_true
+        return np.sum(np.log(1 + np.exp(-margins)))
+    
+
+    def _sigmoid(self, z: np.ndarray) -> np.ndarray:
+        return 1 / (1 + np.exp(-z))
+        
+
+    def predict(self, X: np.ndarray, probabilities: bool = False) -> np.ndarray:
+        if not probabilities:
+            return np.sign(self._sigmoid(self.weights @ X.T))
+        
+        return self._sigmoid(self.weights @ X.T)
+    
