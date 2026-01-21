c41c50c79f1b5fddb294d08520cad88c9df5ac6c
EgorChernov5 <krish19poroh@mail.ru>
2026-01-21 12:13:22 +0300

    Fix data loader

--- students/chernov-ek/lab2/source/dataset.py
+++ students/chernov-ek/lab2/source/dataset.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 import shutil
 import kagglehub
 import pandas as pd
@@ -6,21 +6,16 @@ from sklearn.model_selection import train_test_split
 
 
 def load_iris_dataset(random_state: int = 42):
-    # Download latest version
-    path_save = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
-    
-    file_path = os.path.join(path_save, '2/Iris.csv')
-    if not os.path.exists(file_path):
-        path = kagglehub.dataset_download(handle="uciml/iris")
+    # Download latest version    
+    path_data = Path(kagglehub.dataset_download(handle="uciml/iris"))
+    if path_data.exists():
+        data = pd.read_csv(path_data / "Iris.csv")
+        shutil.rmtree(path_data.parents[2])
 
-        shutil.move(path, path_save)
-        print("Path to dataset files:", path_save)
+        X = data.drop(['Id', 'Species'], axis=1).to_numpy()
+        y = data['Species'].to_numpy()
 
-    data = pd.read_csv(file_path)
-    X = data.drop(['Id', 'Species'], axis=1)
-    y = data['Species']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state, shuffle=True, stratify=y)
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=random_state, shuffle=True, stratify=y_train)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state, shuffle=True, stratify=y)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=random_state, shuffle=True, stratify=y_train)
-
-    return X_train, X_val, X_test, y_train, y_val, y_test
+        return X_train, X_val, X_test, y_train, y_val, y_test
